e30c3378bd1b7eae4622cfdf6ddd89f85e40eeeb
Abdur Rahman <rehmnabdul@gmail.com>
2025-10-30 22:47:15 +0500

    Init Files

---
+++ Plex.NetUI/Plex.NetUI.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Plex.NetUI.Example.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
